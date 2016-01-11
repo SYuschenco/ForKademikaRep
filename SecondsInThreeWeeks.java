f28008c65e9764fa068bbd638f7cef129493ae5f
Sergey <SYuschenco@users.noreply.github.com>
2016-01-11 23:47:16 +0200

    Create SecondsInThreeWeeks.java

---
+++ SecondsInThreeWeeks.java
@@ -0,0 +1,9 @@
+public class SecondsInThreeWeeks {
+
+	public static void main (String[] args){
+		
+	// 3 недели по 7 дней по 24 часа по 60 мин по 60 сек
+	System.out.println(3*7*24*60*60);
+
+	}
+}
