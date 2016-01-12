9d1f1b42d2eabdf8e522b2cffc11d8411867760c
Sergey <SYuschenco@users.noreply.github.com>
2016-01-12 04:36:17 +0200

    Create SubstringReplace.java

---
+++ SubstringReplace.java
@@ -0,0 +1,11 @@
+
+public class WorkWithString {
+
+	public static void main (String[] args){
+		
+		String mtQoute="Let us always meet each other with smile for the smile is the beginning of love.";
+				
+		System.out.println(mtQoute.replace("smile", ":)"));
+			
+	}
+}
