47ea5eec4ead91cd8f35c69df7b39f9df994fde9
Sergey <SYuschenco@users.noreply.github.com>
2016-01-12 03:19:39 +0200

    Create LastCharInTheString.java

---
+++ LastCharInTheString.java
@@ -0,0 +1,15 @@
+
+public class WorkWithString {
+
+	public static void main (String[] args){
+		
+		String mjQoute="I`m failed over and over and over again in my life and that is why I succeed!";
+				
+		int stringLengh=mjQoute.length()-1;
+		
+		char lastChar=mjQoute.charAt(stringLengh);
+				
+		System.out.println(lastChar);
+				
+	}
+}
