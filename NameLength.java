c7c542cf5e4f212bd18c9a1ef6ce95e3dfe9168f
Sergey <SYuschenco@users.noreply.github.com>
2016-01-12 02:52:44 +0200

    Create NameLength.java

---
+++ NameLength.java
@@ -0,0 +1,28 @@
+
+public class WorkWithString {
+
+	static int sum=2;
+	static String s="WWW." ;
+	static String s1="KADEMICA.COM";
+	static String s2="";
+
+	public static void main (String[] args){
+		
+		System.out.println(s);
+		System.out.println(s.length());
+		System.out.println(s1);
+		System.out.println(s+s1);
+		System.out.println(s+s1+"-pro"+100+"super");
+		s+=sum;
+		System.out.println(s);
+		System.out.println(s2);
+		System.out.println("                                                                                        ");
+		System.out.print("1                                                                                        ");
+		System.out.println("                                                                                        ");
+		System.out.println(s.length());
+		System.out.println("Sergey".length());
+		
+		
+		
+	}
+}
