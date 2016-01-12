662212f37edcf8a531df73be09c886427930482d
Sergey <SYuschenco@users.noreply.github.com>
2016-01-12 03:57:06 +0200

    Create Substring.java

---
+++ Substring.java
@@ -0,0 +1,11 @@
+public class WorkWithString {
+
+	public static void main (String[] args){
+		
+		String mjQoute="I`m failed over and over and over again in my life and that is why I succeed!";
+					
+		System.out.println(mjQoute.substring(mjQoute.lastIndexOf("succeed"),mjQoute.lastIndexOf("succeed")+7));
+				
+		
+	}
+}
