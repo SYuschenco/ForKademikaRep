d2d6d325ffb618d9b77f45ae0bddf42028a73995
Sergey <SYuschenco@users.noreply.github.com>
2016-01-12 02:41:34 +0200

    Create WorkWithString.java

---
+++ WorkWithString.java
@@ -0,0 +1,17 @@
+public class WorkWithString {
+
+	static int sum=2;
+	static String s="WWW." ;
+	static String s1="KADEMICA.COM";
+
+	public static void main (String[] args){
+		
+		System.out.println(s);
+		System.out.println(s1);
+		System.out.println(s+s1);
+		System.out.println(s+s1+"-pro"+100+"super");
+		s+=sum;
+		System.out.println(s);
+		System.out.println("                                                                                        ");
+	}
+}
