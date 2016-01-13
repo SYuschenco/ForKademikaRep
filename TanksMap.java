ad2ec3758f625f0f2b5237eee9782f3c75d7efa6
Sergey <SYuschenco@users.noreply.github.com>
2016-01-13 14:03:09 +0200

    Create TanksMap.java

---
+++ TanksMap.java
@@ -0,0 +1,27 @@
+public class TanksMap {
+
+	public static void main (String[] args){
+		
+		//обозначения
+		//C - танки пк
+		//T - танк игрока
+		//E - штаб
+		//W - вода
+		//B - кирпичная стена
+		//R - камни
+		//" " - пусто
+		
+		System.out.println("  1 2 3 4 5 6 7 8 9 ");
+		System.out.println("1|B|B|B|B|B|B|B|B|B|1");
+		System.out.println("2|C| |R| |W| |R| |C|2");
+		System.out.println("3|B|B| |B|B|B| |B|B|3");
+		System.out.println("4|B|B| | |B| | |B|B|4");
+		System.out.println("5| | |B|W|R|W|B| | |5");
+		System.out.println("6|B|B| | | | | |B|B|6");
+		System.out.println("7| | |B|B|B|B|B| | |7");
+		System.out.println("8| |B| |R|T|R| |B| |8");
+		System.out.println("9|B|B|B| |E| |B|B|B|9");
+		System.out.println("  1 2 3 4 5 6 7 8 9 ");
+		
+	}
+}
