fee362aeabea7fa702f2a1df580e38ff7f38f963
Sergey <SYuschenco@users.noreply.github.com>
2016-01-15 12:14:18 +0200

    Create Lessons2p2Kademika.java

---
+++ Lessons2p2Kademika.java
@@ -0,0 +1,16 @@
+public class LessonsKademika {
+	
+	
+	public static void main (String[] args){
+		
+		int a=1;
+		int b=2;
+		sum(a,b);
+		}
+	
+		static void sum(int a, int b){
+			
+			System.out.println(a+b);
+		}
+
+}
