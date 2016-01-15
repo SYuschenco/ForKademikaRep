2a6fe421e316786d0c0ef80943eaeeea94989306
Sergey <SYuschenco@users.noreply.github.com>
2016-01-15 12:09:10 +0200

    Create Lessons2p1Kademika.java

---
+++ Lessons2p1Kademika.java
@@ -0,0 +1,15 @@
+public class LessonsKademika {
+	
+	
+	public static void main (String[] args){
+		
+		String name="Oleg";
+		Hello(name);
+		}
+	
+		static void Hello(String name){
+			
+			System.out.println("Hello "+name);
+		}
+
+}
