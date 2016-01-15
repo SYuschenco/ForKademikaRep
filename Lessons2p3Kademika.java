9f011b5232d521e36139f3db1957e71529ff36dc
Sergey <SYuschenco@users.noreply.github.com>
2016-01-15 12:21:27 +0200

    Create Lessons2p3Kademika.java

---
+++ Lessons2p3Kademika.java
@@ -0,0 +1,14 @@
+public class LessonsKademika {
+	
+	
+	public static void main (String[] args){
+		
+		square(10.0);
+		}
+	
+		static void square(double a){
+			
+			System.out.println(Math.pow(a, 2));
+		}
+
+}
